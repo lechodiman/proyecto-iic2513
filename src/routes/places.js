47ed074e7dd38103390aee281b6f4eeaf3f7373e
IgnacioTobar <iatobar@uc.cl>
2018-04-14 23:21:44 -0300

    Added reviews to places

--- src/routes/places.js
+++ src/routes/places.js
@@ -7,6 +7,19 @@ async function loadPlace(ctx, next) {
   return next();
 }
 
+async function saveReview(ctx, next) {
+  const name = ctx.request.body.name;
+  let comment = ctx.request.body.review;
+  const user = await ctx.orm.user.findOne({ where: {name: name} });
+  if (user) {
+    comment = await ctx.orm.reviewPlace.build({comment});
+    comment = await comment.save();
+    await comment.setUser(user.id);
+    await comment.setPlace(ctx.params.id);
+  }
+  return next();
+}
+
 async function getReviews(ctx, next) {
   ctx.state.reviews = await ctx.orm.reviewPlace.findAll({
                                   attributes: ['id', 'comment', 'userId', 'createdAt'],
@@ -83,6 +96,15 @@ router.del('places.delete', '/:id', loadPlace, async(ctx) => {
 
 
 router.get('places.profile', '/places/:id', loadPlace, getReviews, async(ctx) => {
+  const { place } = ctx.state;
+  await ctx.render('places/profile', {
+    place,
+    reviews: ctx.state.reviews,
+    placePath: place => ctx.router.url('places.profile', { id: place.id }),
+  });
+});
+
+router.post('places.profile', '/places/:id', loadPlace, saveReview, getReviews, async(ctx) => {
   const { place } = ctx.state;
   await ctx.render('places/profile', {
     place,
