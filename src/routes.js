9c48180801dd07d4f37341864dfac56301ae662c
IgnacioTobar <iatobar@uc.cl>
2018-04-29 19:20:51 -0300

    Added user login

--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,19 @@ const places = require('./routes/places');
 const routes = require('./routes/routes');
 const groups = require('./routes/groups');
 const miscs = require('./routes/misc');
-
+const session = require('./routes/session');
 
 const router = new KoaRouter();
+
+router.use(async (ctx, next) => {
+  Object.assign(ctx.state, {
+    currentUser: ctx.session.userId && await ctx.orm.user.findById(ctx.session.userId),
+    newSessionPath: ctx.router.url('session.new'),
+    destroySessionPath: ctx.router.url('session.destroy'),
+    profilePath: user => ctx.router.url('users.profile', { id: user.id }),
+  });
+  return next();
+});
 
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
@@ -18,5 +28,6 @@ router.use('/places', places.routes());
 router.use('/routes', routes.routes());
 router.use('/groups', groups.routes());
 router.use('/misc', miscs.routes());
+router.use('/session', session.routes());
 
 module.exports = router;
