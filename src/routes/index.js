697ade4fefa894febf74fbf8b50e3541d08c5fb9
frvalenzuela <you@example.com>
2018-05-07 16:57:42 -0300

    lint fix and sign up botton

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,20 @@ const pkg = require('../../package.json');
 
 const router = new KoaRouter();
 
+
+router.get('/', async (ctx) => {
+  const users = await ctx.orm.user.findAll();
+  await ctx.render('/index', {
+    users,
+    submitUserPath: ctx.router.url('users.new'),
+    editUserPath: user => ctx.router.url('users.edit', { id: user.id }),
+    deleteUserPath: user => ctx.router.url('users.delete', { id: user.id }),
+    profilePath: user => ctx.router.url('users.profile', { id: user.id }),
+    admin: await ctx.state.isAdmin(),
+  });
+});
+
 router.get('/', async (ctx) => {
   await ctx.render('index', { appVersion: pkg.version });
 });
-
 module.exports = router;
