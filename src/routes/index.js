7233d218012cf5eed54da428f8dd5729d3e6a487
frvalenzuela <frvalenzuela@uc.cl>
2018-05-22 15:30:17 -0400

    fix sing up

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,6 @@ router.get('/', async (ctx) => {
   const users = await ctx.orm.user.findAll();
   await ctx.render('/index', {
     users,
-    submitUserPath: ctx.router.url('users.new'),
     editUserPath: user => ctx.router.url('users.edit', { id: user.id }),
     deleteUserPath: user => ctx.router.url('users.delete', { id: user.id }),
     profilePath: user => ctx.router.url('users.profile', { id: user.id }),
