786f49b7bd32b3fd4b2ba11da6e9783d54ba42dd
IgnacioTobar <iatobar@uc.cl>
2018-06-17 18:39:59 -0400

    Fixed linter errors

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,2 @@
 import '../styles/index.scss';
-import './slider.js';
-
-// console.log('App is running on the browser too!');
+import './slider';
--- src/models/admin.js
+++ src/models/admin.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const admin = sequelize.define('admin', {
     adminId: DataTypes.INTEGER,
   });
-  admin.associate = function associate(models) {
+  admin.associate = function associate() {
     // associations can be defined here
   };
   return admin;
