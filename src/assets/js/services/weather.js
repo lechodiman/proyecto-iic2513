0bd5b5e7b4edfe798db109c5b4368c5b6b771f8f
frvalenzuela <frvalenzuela@uc.cl>
2018-06-21 19:55:29 -0400

    add documentation

--- src/assets/js/services/weather.js
+++ src/assets/js/services/weather.js
@@ -1,5 +1,5 @@
 async function getWeather(location) {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location},cl&appid=5f0011af63d779a94651eea0f1e25e2c`);
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}.cl&appid=5f0011af63d779a94651eea0f1e25e2c`);
   return response.json();
 }
 
